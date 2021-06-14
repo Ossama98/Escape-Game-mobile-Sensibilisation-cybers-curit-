b4d41b9e31c0cca11ed7f6d4c78f1e236bc04e2e
Ralph <ralph.el-chalfoun@etu.unice.fr>
2021-06-15 00:54:37 +0200

    Add files via upload

---
+++ SiteWebVitrine/index.php
@@ -0,0 +1,17 @@
+<?php
+  include_once 'header.php';
+?>
+
+<section class="index-intro">
+  
+</section>
+
+<img class="logo-picture"
+     src="src\Escape Hack blanc y.png">
+
+<section class="index-categories">
+</section>
+
+<?php
+  include_once 'footer.php';
+?>
