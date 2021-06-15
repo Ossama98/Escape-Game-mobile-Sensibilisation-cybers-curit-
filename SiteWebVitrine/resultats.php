430bf69309e39cece2fe249da063c8454857d8df
Ralph <ralph.el-chalfoun@etu.unice.fr>
2021-06-16 00:11:22 +0200

    Add files via upload

--- SiteWebVitrine/resultats.php
+++ SiteWebVitrine/resultats.php
@@ -25,13 +25,13 @@ function afficherLaPageHTML(score){
         document.getElementById("signification").innerHTML = "Malheuresement il va falloir revoir les bases de la cybersécurité, nous vous invitons à lire ce récapitulatif quant aux bonnes pratiques vis-à-vis de la cybersécurité.";
     }
     else if(score == "4" || score == "5" || score == "6"){
-        document.getElementById("signification").innerHTML = "Vos résultats ne sont pas mauvais, mais il est esssentiel de bien comprendre les bases, pour completer vos connaissances nous vous invitons à lire ce récapitulatif quant aux bonnes pratiques vis-à-vis de la cybersécurité.";
+        document.getElementById("signification").innerHTML = "Vos résultats ne sont pas mauvais, mais il est esssentiel de bien comprendre les bases, pour compléter vos connaissances nous vous invitons à lire ce récapitulatif quant aux bonnes pratiques vis-à-vis de la cybersécurité.";
     }
     else if(score == "7" || score == "8"||score == "9"|| score == "10" || score == "11" ||score == "12"){
-        document.getElementById("signification").innerHTML = "Vous vous en êtes bien sorti, mais il reste néanmoins quelques points à améliorer, pour completer vos connaissances nous vous invitons à lire ce récapitulatif quant aux bonnes pratiques vis-à-vis de la cybersécurité.";
+        document.getElementById("signification").innerHTML = "Vous vous en êtes bien sorti, mais il reste néanmoins quelques points à améliorer, pour compléter vos connaissances nous vous invitons à lire ce récapitulatif quant aux bonnes pratiques vis-à-vis de la cybersécurité.";
     }
     else if(score == "13"||score == "14" || score == "15" ||score == "16"){
-        document.getElementById("signification").innerHTML = "Vos performances ont été remarquable, pour completer vos connaissances nous vous invitons à lire ce récapitulatif quant aux bonnes pratiques vis-à-vis de la cybersécurité.";
+        document.getElementById("signification").innerHTML = "Vos performances ont été remarquable, pour compléter vos connaissances nous vous invitons à lire ce récapitulatif quant aux bonnes pratiques vis-à-vis de la cybersécurité.";
     }
 }
 
